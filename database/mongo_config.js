5b90dd208de459ecda435c10bee588d0dca42f77
Yashwant Sahu <77061466+YASH1730@users.noreply.github.com>
2021-08-18 12:04:41 +0530

    Update mongo_config.js

--- database/mongo_config.js
+++ database/mongo_config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const colors = require('colors');
 const mongoose = require('mongoose')
 
-mongoose.connect(`mongodb+srv://PocketChat:process.env.mongo_uri@cluster0.qi4oj.mongodb.net/FAQ?retryWrites=true&w=majority`, { useNewUrlParser : true, useUnifiedTopology : true})
+mongoose.connect(`mongodb+srv://PocketChat:${process.env.mongo_uri}@cluster0.qi4oj.mongodb.net/FAQ?retryWrites=true&w=majority`, { useNewUrlParser : true, useUnifiedTopology : true})
 .then((res)=>console.log('> Connected...'.bgCyan))
 .catch(err=>console.log(`> Error while connecting to mongoDB : ${err.message}`.underline.red ))
 
